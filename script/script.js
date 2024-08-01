3b8032cee3ea4b6791cb78f19d4a15995650fff6
juan-erre <125911752+juan-erre@users.noreply.github.com>
2024-08-01 17:40:38 +0200

    Inicio

---
+++ script/script.js
@@ -0,0 +1,11 @@
+// Recarga la pagina al hacer click en el botón
+document.getElementById("reiniciar_animacion").addEventListener('click',reiniciar,false);
+function reiniciar(){
+    location.reload()                                                                   
+}  
+
+
+
+
+
+
